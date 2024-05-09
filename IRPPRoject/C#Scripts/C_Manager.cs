a4e359a9d8a6901429af6bce6034b0265ec3460a
Jay <91475458+JayK1tsune@users.noreply.github.com>
2024-05-10 00:53:50 +0100

    added c#

---
+++ IRPPRoject/C#Scripts/C_Manager.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+
+public partial class C_Manager : Node
+{
+
+    public override void _Ready()
+    {
+        //print hello world
+        GD.Print("Hello, World!");
+        GD.Print("Test 2!");
+    }
+
+}
